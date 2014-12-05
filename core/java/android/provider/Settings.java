24a1b83bef6ba72cbbae1f91f3bc3afef3d26c07
Steve Kondik <steve@cyngn.com>
2014-12-05 08:31:29 -0800

    SystemUI: Forward-port notification counters [1/2]
    Change-Id: Ia8609c56a523906404a9155cea6987f40c10f30d
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5835,6 +5835,12 @@ public final class Settings {
         public static final String STATUSBAR_COLORED_ICONS = "statusbar_colored_icons";
 
         /**
+         * Show the pending notification counts as overlays on the status bar
+         * @hide
+         */
+        public static final String STATUSBAR_NOTIF_COUNT = "statusbar_notif_count";
+
+        /**
          * Whether to display cross sign for a data disabled connection
          * @hide
          */
