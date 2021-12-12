4c910a4a9b54c8f5151a0e930a4ac9009bb1d1a8
riteshm321 <riteshm321@gmail.com>
2021-12-12 08:42:57 +0530

    [SQUASH] Revert Pixel Props Utils
    Revert "base: PixelPropsUtils: Add Calculator to party"
    
    This reverts commit 63bba5325a699a312f15e658d6c4e76302736cac.
    
    Revert "base: PixelPropsUtils: Add Gmail & Chrome to party"
    
    This reverts commit 9c1cd398220fb5e681d9968593119420e5df6b8c.
    
    Revert "base: PixelPropsUtils: Add Google Assistant to Enable 2.0 UI"
    
    This reverts commit 9fb8a4065929f0f99efcd0974b670d1d596b0dda.
    
    Revert "base: PixelPropsUtils: Update Fingerprint To October Security Patch"
    
    This reverts commit 06114f9fc1a051956046cb1cd89885eba4da9d2f.
    
    Revert "base: PixelPropsUtils: Add Material You redesigned apps to spoof list."
    
    This reverts commit 09230865bba92ea4f7eb3fc71137729b4818446d.
    
    Revert "PixelPropsUtils: Don't set model prop if gms"
    
    This reverts commit 8748bb79c3f6af73fe3488013cb4d21bba536293.
    
    Revert "PixelPropsUtils: Spoof Pixel XL for Google Photos"
    
    This reverts commit 05683faca15b552337d6d431728eed196c71f1c5.
    
    Revert "PixelPropsUtils: add Google Play Store to party as well"
    
    This reverts commit 2d2d9ab148c769d0ab7403bae90f231c495b9b75.
    
    Revert "PixelPropsUtils: Update packages"
    
    This reverts commit 59f89882ee8c5f898ce0a7f72666cb9d7ba074c5.
    
    Revert "PixelPropsUtils: Set redfin build fingerprint"
    
    This reverts commit c4ae4f1488c4296383fa56dfc86ed98c16e08b9f.
    
    Revert "PixelPropsUtils: Switch back to Pixel 5 props for all apps"
    
    This reverts commit 39500592de5ffb89d0c641f5c8e939414cb10cfa.
    
    Revert "PixelPropsUtils: Set pixel 5 props for specific apps"
    
    This reverts commit b3ae41e7678ddeaa765e498c3e48e097858f7020.
    
    Revert "PixelPropsUtils: Change props to crosshatch"
    
    This reverts commit 439dc12836ab6c9ed5f869161979f539f6c80629.
    
    Revert "PixelPropsUtils: bring back wallpaper collection"
    
    This reverts commit c197ddc8fc0ea742bf6a51780b6323a1ab9051fb.
    
    Revert "PixelPropsUtils: Cleanup apps"
    
    This reverts commit a3a4d17999d1a5cbb3e307b795d921c604b660ae.
    
    Revert "base: PixelPropsUtils: Don't set build type/build debug props"
    
    This reverts commit 6e230254f968b382669ad5bfee080f046c047541.
    
    Revert "base: PixelPropsUtils: Override fingerprint for SettingsIntelligence"
    
    This reverts commit 88c1466838279610e8d1282e44409ad123f4f19b.
    
    Change-Id: Iab2d93de95106ea38198823d200856fdad507a38
    
    Revert "base: Introduce PixelPropsUtils"
    
    This reverts commit 6d2fd6d2e2c9243993e665841e1eaabafd73bb72.
    
    Revert "PixelPropsUtils: Make build property spoofing more reliable"
    
    This reverts commit 93b81af9f435dcdb0fd77682eac690845cbd7dd6.
    
    Revert "Spoof build fingerprint for Google Play Services"
    
    This reverts commit d345a238b05cba19b4d1b9f41f6009874a6900f7.
    
    Change-Id: I314a379772b94e32395311cf97afeddd8fa10773
    
    Revert "gmscompat: Add compatibility layer for unprivileged Google Play services"
    
    This reverts commit 76781915af29f28d67dd1f2e1a1075109de02e68.

--- core/java/android/app/Instrumentation.java
+++ core/java/android/app/Instrumentation.java
@@ -57,17 +57,12 @@ import android.view.WindowManagerGlobal;
 
 import com.android.internal.content.ReferrerIntent;
 
-import com.android.internal.gmscompat.AttestationHooks;
-import com.android.internal.gmscompat.GmsHooks;
-
 import java.io.File;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeoutException;
-
-import com.android.internal.util.custom.PixelPropsUtils;
 
 /**
  * Base class for implementing application instrumentation code.  When running
@@ -1181,10 +1176,6 @@ public class Instrumentation {
         Application app = getFactory(context.getPackageName())
                 .instantiateApplication(cl, className);
         app.attach(context);
-        GmsHooks.initApplicationBeforeOnCreate(app);
-        AttestationHooks.initApplicationBeforeOnCreate(app);
-        String packageName = app.getPackageName();
-        PixelPropsUtils.setProps(packageName);
         return app;
     }
     
@@ -1202,10 +1193,6 @@ public class Instrumentation {
             ClassNotFoundException {
         Application app = (Application)clazz.newInstance();
         app.attach(context);
-        GmsHooks.initApplicationBeforeOnCreate(app);
-        AttestationHooks.initApplicationBeforeOnCreate(app);
-        String packageName = app.getPackageName();
-        PixelPropsUtils.setProps(packageName);
         return app;
     }
 
