2ed6b4d5e5b20521e648e9924ce064d566b9a149
Trishiraj <gamerprince.exp@gmail.com>
2022-01-08 14:50:20 +0000

    Add back our ThemeUtils
    Might need some cleanup, Will look into it later
    
    Signed-off-by: Trishiraj <gamerprince.exp@gmail.com>
    Change-Id: Ib1268e1128172e692defead27528e363a6a3c651

---
+++ core/java/com/android/internal/util/corvus/ThemesUtils.java
@@ -0,0 +1,150 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *               2020-2022 CorvusRom
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.util.corvus;
+
+public class ThemesUtils {
+
+    public static final String[] ADAPTIVE_ICON_SHAPE = {
+            "com.android.theme.icon.teardrop",
+            "com.android.theme.icon.squircle",
+            "com.android.theme.icon.roundedrect",
+            "com.android.theme.icon.cylinder",
+            "com.android.theme.icon.hexagon",
+            "com.android.theme.icon.roundedhexagon",
+            "com.android.theme.icon.pebble",
+            "com.android.theme.icon.tapered_rect",
+            "com.android.theme.icon.vessel",
+    };
+
+    public static final String[] PITCH_BLACK = {
+            "com.android.theme.pitchblack.system",
+            "com.android.theme.pitchblack.systemui",
+    };
+
+    public static final String[] SOLARIZED_DARK = {
+            "com.android.theme.solarizeddark.system",
+            "com.android.theme.solarizeddark.systemui",
+    };
+
+    public static final String[] DARK_GREY = {
+            "com.android.theme.darkgrey.system",
+            "com.android.theme.darkgrey.systemui",
+    };
+
+    public static final String[] MATERIAL_OCEAN = {
+            "com.android.theme.materialocean.system",
+            "com.android.theme.materialocean.systemui",
+    };
+
+    public static final String[] STATUSBAR_ICONS = {
+            "com.android.theme.icon_pack.filled.android",
+            "com.android.theme.icon_pack.rounded.android",
+            "com.android.theme.icon_pack.circular.android",
+            "com.android.theme.icon_pack.kai.android",
+            "com.android.theme.icon_pack.sam.android",
+            "com.android.theme.icon_pack.victor.android",
+    };
+
+    public static final String[] CHOCO_X = {
+            "com.android.theme.chocox.system",
+            "com.android.theme.chocox.systemui",
+    };
+
+    public static final String[] BAKED_GREEN = {
+            "com.android.theme.bakedgreen.system",
+            "com.android.theme.bakedgreen.systemui",
+    };
+
+    public static final String[] CORVUS_CLEAR = {
+            "com.android.theme.corvusclear.system",
+            "com.android.theme.corvusclear.systemui",
+    };
+
+    public static final String[] NAVBAR_STYLES = {
+            "com.android.theme.navbar.asus",
+            "com.android.theme.navbar.oneplus",
+            "com.android.theme.navbar.oneui",
+            "com.android.theme.navbar.tecno",
+            "com.android.theme.navbar.android",
+            "com.android.theme.navbar.moto",
+            "com.android.theme.navbar.nexus",
+            "com.android.theme.navbar.old",
+            "com.android.theme.navbar.sammy",
+    };
+
+    public static final String[] QS_HEADER_THEMES = {
+            "com.android.systemui.qsheader.grey",
+            "com.android.systemui.qsheader.lightgrey",
+            "com.android.systemui.qsheader.accent",
+            "com.android.systemui.qsheader.transparent",
+    };
+
+    public static final String[] SWITCH_STYLE = {
+        "com.android.system.switch.md2", // 1
+        "com.android.system.switch.oneplus", // 2
+        "com.android.system.switch.telegram", // 3
+        "com.android.system.switch.narrow", // 4
+        "com.android.system.switch.contained", // 5
+        "com.android.system.switch.retro", // 6
+        "com.android.system.switch.stockish", // 7
+        "com.android.system.switch.android12", // 8
+        "com.android.system.switch.oos", // 9
+    };
+
+    public static final String[] STATUSBAR_HEIGHT = {
+            "com.gnonymous.gvisualmod.sbh_m", // 1
+            "com.gnonymous.gvisualmod.sbh_l", // 2
+            "com.gnonymous.gvisualmod.sbh_xl", // 3
+    };
+
+    public static final String[] UI_RADIUS = {
+            "com.gnonymous.gvisualmod.urm_r", // 1
+            "com.gnonymous.gvisualmod.urm_m", // 2
+            "com.gnonymous.gvisualmod.urm_l", // 3
+    };
+
+    public static final String[] QS_TILE_THEMES = {
+            "com.android.systemui.qstile.default",
+            "com.android.systemui.qstile.circletrim",
+            "com.android.systemui.qstile.dualtonecircletrim",
+            "com.android.systemui.qstile.squircletrim",
+            "com.android.systemui.qstile.wavey", // 4
+            "com.android.systemui.qstile.pokesign", // 5
+            "com.android.systemui.qstile.ninja", // 6
+            "com.android.systemui.qstile.dottedcircle", // 7
+            "com.android.systemui.qstile.attemptmountain", // 8
+            "com.android.systemui.qstile.squaremedo", // 9
+            "com.android.systemui.qstile.inkdrop", // 10
+            "com.android.systemui.qstile.cookie", // 11
+            "com.android.systemui.qstile.circleoutline", //12
+            "com.android.systemui.qstile.neonlike", // 13
+            "com.android.systemui.qstile.oos", // 14
+            "com.android.systemui.qstile.triangles", // 15
+            "com.android.systemui.qstile.divided", // 16
+            "com.android.systemui.qstile.cosmos", // 17
+            "com.android.systemui.qstile.squircle", // 18
+	          "com.android.systemui.qstile.teardrop", // 19
+            "com.android.systemui.qstile.deletround", //20
+            "com.android.systemui.qstile.hexagon", // 21
+            "com.android.systemui.qstile.diamond", // 22
+            "com.android.systemui.qstile.star", // 23
+            "com.android.systemui.qstile.gear", // 24
+            "com.android.systemui.qstile.badge", // 25
+            "com.android.systemui.qstile.badgetwo", // 26
+    };
+}
